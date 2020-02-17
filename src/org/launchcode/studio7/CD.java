3688ee829bbbd35d51f2b04fdcdada523cabfeb2
Laura Creason <creason4@gmail.com>
2020-02-16 20:15:51 -0600

    studio-8

--- src/org/launchcode/studio7/CD.java
+++ src/org/launchcode/studio7/CD.java
@@ -1,10 +1,24 @@
 package org.launchcode.studio7;
 
-public class CD {
+public class CD extends BaseDisc implements OpticalDisc {
+
+    public CD(String aName, int maxCapacity, String aType, int someUsedCapacity) {
+        super(aName, maxCapacity, aType, someUsedCapacity);
+    }
 
     // TODO: Implement your custom interface.
 
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
 
+    @Override
+    public void spinDisc() {
+        System.out.println("A CD spins at a rate of 200 - 500 rpm.");
+    }
+
+
+    @Override
+    public void readData() {
+        System.out.println("Would you like to play a game?");
+    }
 }
